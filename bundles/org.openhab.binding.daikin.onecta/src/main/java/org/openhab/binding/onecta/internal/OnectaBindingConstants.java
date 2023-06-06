51dce05e7ab946cf6cb2ef0c2bbf3f25be88cc69
alex <alex@drent-ict.nl>
2023-06-06 08:21:18 +0200

    First steps

---
+++ bundles/org.openhab.binding.daikin.onecta/src/main/java/org/openhab/binding/onecta/internal/OnectaBindingConstants.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2010-2023 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.onecta.internal;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.openhab.core.thing.ThingTypeUID;
+
+/**
+ * The {@link OnectaBindingConstants} class defines common constants, which are
+ * used across the whole binding.
+ *
+ * @author Alexander Drent - Initial contribution
+ */
+@NonNullByDefault
+public class OnectaBindingConstants {
+
+    private static final String BINDING_ID = "onecta";
+    public static final String BRIDGE = "account";
+    // List of all Device Types
+    public static final String DEVICE = "device";
+
+    // List of all Thing Type UIDs
+    public static final ThingTypeUID THING_TYPE_SAMPLE = new ThingTypeUID(BINDING_ID, "sample");
+
+    // List of Bridge Type
+
+    // List of all Bridge Thing Type UIDs
+    public static final ThingTypeUID BRIDGE_THING_TYPE = new ThingTypeUID(BINDING_ID, BRIDGE);
+
+    // List of all Thing Type UIDs
+    public static final ThingTypeUID DEVICE_THING_TYPE = new ThingTypeUID(BINDING_ID, DEVICE);
+
+    // List of all Channel ids
+    public static final String CHANNEL_1 = "channel1";
+    public static final String HOMEKITMODE = "homekitmode";
+}
