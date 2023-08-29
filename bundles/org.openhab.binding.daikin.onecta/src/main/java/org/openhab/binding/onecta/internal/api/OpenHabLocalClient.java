cedb2f8c607d2979acb42383916868195b46f517
Alexander Drent <81358972+adr001db@users.noreply.github.com>
2023-08-29 21:30:46 +0200

    Add Update na 2FA

--- bundles/org.openhab.binding.daikin.onecta/src/main/java/org/openhab/binding/onecta/internal/api/OpenHabLocalClient.java
+++ bundles/org.openhab.binding.daikin.onecta/src/main/java/org/openhab/binding/onecta/internal/api/OpenHabLocalClient.java
@@ -1,41 +1,45 @@
 package org.openhab.binding.onecta.internal.api;
 
+import org.eclipse.jetty.http.HttpHeader;
+import org.openhab.binding.onecta.internal.exception.DaikinCommunicationException;
+import org.openhab.core.io.net.http.HttpClientFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class OpenHabLocalClient {
-    /*
-     * private final Logger logger = LoggerFactory.getLogger(OnectaSignInClient.class);
-     * 
-     * private HttpClient httpClient;
-     * private String LocalHostUrl;
-     * private String apiToken;
-     * 
-     * 
-     * public OpenHabLocalClient(HttpClientFactory httpClientFactory, String localHostUrl, String apiToken) {
-     * this.httpClient = httpClientFactory.getCommonHttpClient();
-     * this.apiToken = apiToken;
-     * this.LocalHostUrl = localHostUrl;
-     * }
-     * 
-     * public Response doBearerRequestGet(Boolean refreshed) throws DaikinCommunicationException {
-     * Response response = null;
-     * HttpClient httpClient = new HttpClient();
-     * // logger.debug(String.format("doBearerRequestGet : refershed %s", refreshed.toString()));
-     * try {
-     * response = httpClient.newRequest(OnectaProperties.getBaseUrl("")).method(HttpMethod.GET)
-     * .header(HttpHeader.AUTHORIZATION, String.format("Bearer %s", onectaSignInClient.getToken()))
-     * .send();
-     * } catch (Exception e) {
-     * if (!refreshed) {
-     * try {
-     * logger.debug(String.format("Get new token"));
-     * onectaSignInClient.fetchAccessToken();
-     * response = doBearerRequestGet(true);
-     * } catch (DaikinCommunicationException ex) {
-     * throw new DaikinCommunicationException(ex);
-     * }
-     * }
-     * }
-     * 
-     * return response;
-     * }
-     */
+      private final Logger logger = LoggerFactory.getLogger(OnectaSignInClient.class);
+
+      private HttpClient httpClient;
+      private String LocalHostUrl;
+      private String apiToken;
+
+
+      public OpenHabLocalClient(HttpClientFactory httpClientFactory, String localHostUrl, String apiToken) {
+      this.httpClient = httpClientFactory.getCommonHttpClient();
+      this.apiToken = apiToken;
+      this.LocalHostUrl = localHostUrl;
+      }
+
+      public Response doBearerRequestGet(Boolean refreshed) throws DaikinCommunicationException {
+      Response response = null;
+      HttpClient httpClient = new HttpClient();
+      // logger.debug(String.format("doBearerRequestGet : refershed %s", refreshed.toString()));
+      try {
+      response = httpClient.newRequest(OnectaProperties.getBaseUrl("")).method(HttpMethod.GET)
+      .header(HttpHeader.AUTHORIZATION, String.format("Bearer %s", onectaSignInClient.getToken()))
+      .send();
+      } catch (Exception e) {
+      if (!refreshed) {
+      try {
+      logger.debug(String.format("Get new token"));
+      onectaSignInClient.fetchAccessToken();
+      response = doBearerRequestGet(true);
+      } catch (DaikinCommunicationException ex) {
+      throw new DaikinCommunicationException(ex);
+      }
+      }
+      }
+
+      return response;
+      }
 }
